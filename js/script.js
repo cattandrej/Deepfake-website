5ce7378255669d3a5b4afd7327a905124509b80e
cattandrej <45920569+cattandrej@users.noreply.github.com>
2020-01-27 11:28:42 +0100

    Basic left-right scroll added

---
+++ js/script.js
@@ -0,0 +1,47 @@
+console.log("HELLO");
+
+var delta = 0;
+var wheelEventTime = Date.now();
+
+var sections = ["first",
+    "second",
+    "third"];
+var currentSection = 0;
+
+function scroll_to(id) {
+    $('html,body').animate({
+        scrollTop: $('#' + id).offset().top
+    }, 'slow');
+}
+
+window.addEventListener("wheel", event => {
+    
+    if (Date.now() - wheelEventTime > 500) {
+        delta = Math.sign(event.deltaY);
+        console.log(event.deltaY    );
+
+        if (delta > 0) {
+            if (currentSection < sections.length - 1) {
+                currentSection++;
+            }
+        }
+
+        if (delta < 0) {
+            if (currentSection > 0) {
+                currentSection--;
+            }
+        }
+
+        scroll_to(sections[currentSection]);
+    }
+    wheelEventTime = Date.now();
+});
+
+var dir = 1;
+
+$(document).ready(function() { 
+    $(".button").click(function() { 
+        $("div").scrollLeft(2000 * dir);  
+        dir *= (-1);
+    }); 
+}); 
