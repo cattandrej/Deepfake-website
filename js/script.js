3bd6ead9aa5b10ef659d02d260d52c2ff8337e97
cattandrej <45920569+cattandrej@users.noreply.github.com>
2020-01-31 19:50:58 +0100

    fix intro video

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ var sectionList = [
     new Section(_sections[5], _experiment, _fixed, _visible),
     new Section(_sections[6], _defaul, _resizable, _visible),
     new Section(_sections[7], _defaul, _fixed, _visible),
-    new Section(_sections[8], _defaul, _fixed, _visible),
+    new Section(_sections[8], _defaul, _resizable, _visible),
     new Section(_sections[9], _experiment, _fixed, _visible),
     new Section(_sections[10], _experiment, _fixed, _visible),
     new Section(_sections[11], _defaul, _fixed, _visible),
